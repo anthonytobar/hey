ac75171272e168062e81aa145490df0c8c805648
anthonytobar <37843535+anthonytobar@users.noreply.github.com>
2020-02-13 15:44:55 -0800

    Add files via upload

---
+++ heyl.rb
@@ -0,0 +1,26 @@
+use_bpm 112
+use_synth :piano
+a=0.5
+define :ht do|b, c, d|
+  play b
+  sleep a
+  play c
+  play d
+  sleep a
+end
+ht :d, :a, :b
+ht :d, :a, :b
+ht :d, :a, :b
+ht :d, :a, :b
+ht :f, :c, :b
+ht :f, :c, :b
+ht :f, :c, :b
+ht :f, :c, :b
+ht :d, :a, :b
+ht :d, :a, :b
+ht :d, :a, :b
+ht :d, :a, :b
+ht :f, :c, :b
+ht :f, :c, :b
+ht :f, :c, :b
+ht :f, :c, :b
